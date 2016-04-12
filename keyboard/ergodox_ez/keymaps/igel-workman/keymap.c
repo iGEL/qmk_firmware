8bb22761a531d92bd1a223a2b002008354218cf9
Johannes Barre <igel@igels.net>
2016-04-13 00:00:30 +0200

    iGEL-workman added

---
+++ keyboard/ergodox_ez/keymaps/igel-workman/keymap.c
@@ -0,0 +1,227 @@
+#include "ergodox_ez.h"
+#include "debug.h"
+#include "action_layer.h"
+
+#define BASE 0 // default layer
+#define QWER 1 // QWERTY
+#define SYMB 2 // symbols
+#define MDIA 3 // media keys
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Keymap 0: Basic layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * | Esc    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   L3   |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   D  |   R  |   W  |   B  |  L1  |           |  L1  |   J  |   F  |   U  |   P  |   '  |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LCtrl  |   A  |   S  |   H  |   T  |   G  |------|           |------|   Y  |   N  |   E  |   O  |   I  | RCtrl  |
+ * |--------+------+------+------+------+------| Hyper|           |  L3  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   M  |   C  |   V  |      |           |      |   K  |   L  |   ,  |   .  |   /  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | LGui |   Ä  |   Ö  | Left | Right|                                       |  Up  | Down |   Ü  |   ß  | RGui |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | Del  | Ins  |       | PgUp | PgDn |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      | LGui | LAlt |       | RAlt |        |      |
+ *                                 |Backsp|  /   |------|       |------| Enter  |Space |
+ *                                 |ace   | Esc  | ~L2  |       | ~L2  |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+// If it accepts an argument (i.e, is a function), it doesn't need KC_.
+// Otherwise, it needs KC_*
+[BASE] = KEYMAP(  // layer 0 : default workman
+        // left hand
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_TAB,         KC_Q,         KC_D,   KC_R,   KC_W,   KC_B,   TG(QWER),
+        KC_LCTL,        KC_A,         KC_S,   KC_H,   KC_T,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_M,   KC_C,   KC_V,   ALL_T(KC_NO),
+        KC_LGUI,        KC_TRNS,      KC_TRNS,KC_LEFT,KC_RGHT,
+                                                      KC_DELT,KC_INS,
+                                                              KC_LALT,
+                                              KC_BSPC,GUI_T(KC_ESC),MO(SYMB),
+        // right hand
+             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             TG(MDIA),
+             TG(QWER),    KC_J,   KC_F,   KC_U,   KC_P,   KC_QUOT,          KC_BSLS,
+                          KC_Y,   KC_N,   KC_E,   KC_O,   KC_I,             KC_RCTL,
+             TG(MDIA),    KC_K,   KC_L,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                                  KC_UP,  KC_DOWN,KC_TRNS,KC_TRNS,          KC_RGUI,
+             KC_PGUP,     KC_PGDN,
+             KC_RALT,
+             MO(SYMB),KC_ENT, KC_SPC
+    ),
+/* Keymap 1: QWERTY layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * | Esc    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   L3   |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | RCtrl  |
+ * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | LGui |   Ä  |   Ö  | Left | Right|                                       |  Up  | Down |   Ü  |   ß  | RGui |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | Del  | Ins  |       | PgUp | PpDn |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 |      | LGui | LAlt |       | RAlt |        |      |
+ *                                 |Backsp|  /   |------|       |------| Enter  |Space |
+ *                                 |ace   | Esc  | ~L2  |       | ~L2  |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[QWER] = KEYMAP(  // layer 1 : QWERTY
+        // left hand
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(QWER),
+        KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LGUI,        KC_TRNS,      KC_TRNS,KC_LEFT,KC_RGHT,
+                                                      KC_DELT,KC_INS,
+                                                              KC_LALT,
+                                               KC_BSPC,GUI_T(KC_ESC),MO(SYMB),
+        // right hand
+             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             TG(MDIA),
+             TG(QWER),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_RCTL,
+             TG(MDIA),    KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                                  KC_UP,  KC_DOWN,KC_TRNS,KC_TRNS,          KC_RGUI,
+             KC_PGUP,        KC_PGDN,
+             KC_RALT,
+             MO(SYMB),KC_ENT, KC_SPC
+    ),
+/* Keymap 2: Symbol Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |  F11 |           |  F12 |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |   @  |   =  |   {  |   }  |   |  |      |           |      |   `  |   ^  |   ~  |   &  |   /  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   _  |   -  |   (  |   )  |   !  |------|           |------|   H  |   J  |   K  |   L  |   ;  |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |   +  |   *  |   [  |   ]  |   \  |      |           |      |   €  |   #  |   $  |   %  |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |   1  |   2  |   3  |   4  |   5  |                                       |   6  |   7  |   8  |   9  |   0  |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+
+// SYMBOLS
+[SYMB] = KEYMAP(
+       // left hand
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
+       KC_TRNS,KC_AT,  KC_EQL, KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       KC_TRNS,KC_UNDS,KC_MINS,KC_LPRN,KC_RPRN,KC_EXLM,
+       KC_TRNS,KC_PLUS,KC_ASTR,KC_LBRC,KC_RBRC,KC_BSLS,KC_TRNS,
+       KC_1,   KC_2,   KC_3,   KC_4,   KC_5,
+                                       KC_TRNS,KC_TRNS,
+                                               KC_TRNS,
+                               KC_TRNS,KC_COLN,KC_TRNS,
+       // right hand
+       KC_F12,  KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
+       KC_TRNS, KC_GRV,  KC_CIRC,KC_TILD, KC_AMPR, KC_SLSH, KC_TRNS,
+                KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_HASH,KC_DLR,  KC_PERC, KC_TRNS, KC_TRNS,
+                         KC_6,   KC_7,    KC_8,    KC_9,    KC_0,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS
+),
+/* Keymap 2: Media and mouse keys
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      | Lclk | MsUp | Rclk |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
+ *   `----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |Brwser|
+ *                                 |      |      |------|       |------|      |Back  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+// MEDIA AND MOUSE
+[MDIA] = KEYMAP(
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,
+                                           KC_TRNS, KC_TRNS,
+                                                    KC_TRNS,
+                                  KC_TRNS, KC_TRNS, KC_TRNS,
+    // right hand
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
+                          KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS,
+       KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_WBAK
+),
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+};
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+  // MACRODOWN only works in this function
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          register_code(KC_RSFT);
+        } else {
+          unregister_code(KC_RSFT);
+        }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+
+};
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+
+    uint8_t layer = biton32(layer_state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+      // TODO: Make this relevant to the ErgoDox EZ.
+        case 1:
+            ergodox_right_led_1_on();
+            break;
+        case 2:
+            ergodox_right_led_2_on();
+            break;
+        default:
+            // none
+            break;
+    }
+
+};
---
+++ keyboard/ergodox_ez/keymaps/igel-workman/readme.md
@@ -0,0 +1,34 @@
+## iGEL's workman interpretation
+
+This is my ErgoDox interpretation of the
+(Workman layout)[http://www.workmanlayout.com/blog/]. It has a symbol layer
+with VIM movement buttons (H, J, K, L) and features German umlauts[^1] even
+though it's supposed to be used with US-Qwerty settings. They type RAlt + U and
+then `a` for ä etc.
+
+The symbol layer is activated by holding innermost key in the bottom line of
+either half. I really love it, that's why I gave it such a prominent position.
+It also has a QWERTY layer, so you could play games or let your friends try
+your weird keyboard.
+
+[^1]: Not yet working :'(
+
+### Changelog
+#### 2016-04-12
+
+I ported my [old layout](https://github.com/iGEL/ergodox-firmware) over to the
+qmt firmware and applied these changes:
+
+* The middle big key on the left thumb cluster now triggers ESC when tapped.
+  When hold down, it still triggers LGui.
+* Cursor keys are now split between both halves. Left/right are now on the left
+  side, up/down still on the right. I always confused them, hopefully this is
+  easier to type.  Ü and ß moved to the right half because of that.
+* Most positions on the symbol layer changed. All three bracket types are now on
+  the left half, in column 3 & 4, like with the original ErgoDox EZ layout. Most
+  symbols are rearranged because of that, but I kept the VIM movement row (right
+  home row)
+* Added the media layer, with working sound buttons :D
+* Some of the keys I never used changed (in the middle)
+
+* Not yet working: German umlauts and the Euro sign
